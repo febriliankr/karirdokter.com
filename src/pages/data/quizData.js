2abfe3af46c99cf8599a0514047bd5f8e5a1654e
febriliankr <febrilian.kr@gmail.com>
2020-09-22 00:21:41 +0700

    added some more data

--- src/pages/data/quizData.js
+++ src/pages/data/quizData.js
@@ -8,17 +8,17 @@ const questions = [
       "ISTP",
       "ISFP",
       "INFJ",
-      "ESTJ",
-      "ESFJ",
-      "ENFP",
       "INTJ",
       "INFP",
       "INTP",
-      "ESTP",
-      "ESFP",
       "ENTP",
       "ENFJ",
       "ENTJ",
+      "ENFP",
+      "ESTP",
+      "ESFP",
+      "ESTJ",
+      "ESFJ",
     ],
   },
   {
@@ -37,7 +37,10 @@ const questions = [
   {
     section: "Section 3A",
     question: "Kamu adalah orang yang cenderung ...",
-    options: ["Menyesuaikan diri dan suka terhadap aturan", "Memberontak dan tidak suka diatur"],
+    options: [
+      "Menyesuaikan diri dan suka terhadap aturan",
+      "Memberontak dan tidak suka diatur",
+    ],
   },
   {
     section: "Section 3A",
@@ -58,29 +61,31 @@ const questions = [
     section: "Section 3A",
     question: "Kamu adalah orang yang cenderung ...",
     options: ["Mudah stress dan tidak puas", "Santai dan tenang"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka untuk menyenangkan orang lain?",
     options: ["Ya", "Tidak"],
-    description: "(misal : membantu teman, mudah memaafkan atau peduli pada sesama)",
-  },  
+    description:
+      "(misal : membantu teman, mudah memaafkan atau peduli pada sesama)",
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka bergaul dengan lawan jenis?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka untuk bersifat mandiri ",
     options: ["Ya", "Tidak"],
-    description: "(misal : ingin kebebasan, suka mengerjakan apa yang diinginkan, dsb)",
-  },  
+    description:
+      "(misal : ingin kebebasan, suka mengerjakan apa yang diinginkan, dsb)",
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa berguna, percaya dan empati pada orang lain",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka mengalah?",
@@ -90,70 +95,105 @@ const questions = [
     section: "Section 3B",
     question: "Apakah kamu merasa suka mudah terharu?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka untuk menguasai orang lain?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka bekerja secara teratur?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka menonjolkan diri?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka mendapatkan pengakuan secara sosial?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka menyerang/mengkritik orang lain?",
     options: ["Ya", "Tidak"],
-  },  
+  },
   {
     section: "Section 3B",
     question: "Apakah kamu merasa suka membela diri?",
     options: ["Ya", "Tidak"],
-  }, 
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
     options: ["Bekerja dengan orang", "Merawat dengan orang"],
-  }, 
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
     options: ["Bekerja secara luas", "Bekerja secara spesifik"],
-  }, 
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
-    options: ["Terlibat dalam kehidupan seseorang", "Tidak terlalu terlibat dalam kehidupan seseorang"],
-  }, 
+    options: [
+      "Terlibat dalam kehidupan seseorang",
+      "Tidak terlalu terlibat dalam kehidupan seseorang",
+    ],
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
     options: ["Menangani pasien pertama kali", "Menangani pasien rujukan"],
-  }, 
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
-    options: ["Menangani pasien dalam jangka panjang", "Menangani pasien dalam jangka pendek/langsung"],
-  }, 
+    options: [
+      "Menangani pasien dalam jangka panjang",
+      "Menangani pasien dalam jangka pendek/langsung",
+    ],
+  },
   {
     section: "Section 3C",
     question: "Kamu suka",
-    options: ["Menangani masalah umum", "Menangani masalah tidak umum (spesifik)"],
-  }, 
-  
-  
-  
+    options: [
+      "Menangani masalah umum",
+      "Menangani masalah tidak umum (spesifik)",
+    ],
+  },
+  {
+    section: "Section 3C",
+    question: "Kamu suka",
+    options: ["Bekerja di rumah sakit", "Bekerja di klinik"],
+  },
+  {
+    section: "Section 3C",
+    question: "Kamu suka",
+    options: ["Mengumpulkan informasi", "Memberikan masukan/konsultasi"],
+  },
+  {
+    section: "Section 3C",
+    question: "Kamu suka",
+    options: [
+      "Tidak terlalu peduli dengan perkembangan teknologi",
+      "peduli dengan teknologi",
+    ],
+  },
+  // SECTION 4 JOB PREFERENCE
+  {
+    section: "Section 4",
+    question: "Jenis kelamin anda",
+    options: ["Laki-laki", "Perempuan"],
+  },
+  {
+    section: "Section 4",
+    question: "Berapa jam anda ingin bekerja dalam satu hari?",
+    options: ["6 jam", "7 jam", "8 jam", "9 jam"],
+  },
 ];
 
 export default questions;
